b7a991e12d202c050208e004c17e957dd4322451
Mohamed ElSerngawy <m.elserngawy@gmail.com>
2018-01-16 12:07:13 -0500

    Fix install OVS in the vagrant example
    Fix install OVS script in the vagrant example and
    use OVS version 2.8 not upstream version
    
    Change-Id: Ie199ccc7fbdae8aff9d51dc3b4766b30ef2122c6
    Signed-off-by: Mohamed ElSerngawy <m.elserngawy@gmail.com>

--- resources/K8s-ODL-Vagrant/provisioning/setup-Req.sh
+++ resources/K8s-ODL-Vagrant/provisioning/setup-Req.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-ODL_IP=$1
-
 # Install OVS and dependencies
-sudo apt-get build-dep dkms
-sudo apt-get install -y autoconf automake bzip2 debhelper dh-autoreconf \
-                        libssl-dev libtool openssl procps python-six dkms
-git clone https://github.com/openvswitch/ovs.git
+sudo apt-get update
+sudo apt-get install -y dh-autoreconf debhelper autoconf automake libssl-dev pkg-config bzip2 openssl python-all procps python-qt4 python-zopeinterface python-twisted-conch
+
+git clone https://github.com/openvswitch/ovs.git -b branch-2.8
 pushd ovs/
 sudo DEB_BUILD_OPTIONS='nocheck parallel=2' fakeroot debian/rules binary
 popd
-sudo dpkg -i openvswitch-datapath-dkms*.deb
-sudo dpkg -i openvswitch-switch*.deb openvswitch-common*.deb \
-             python-openvswitch*.deb libopenvswitch*.deb
+sudo dpkg -i openvswitch-datapath-dkms*.deb openvswitch-switch*.deb openvswitch-common*.deb python-openvswitch*.deb libopenvswitch*.deb
 
 sudo rm -rf *.deb
 
@@ -32,12 +28,3 @@ sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-ke
 sudo su -c "echo \"deb http://apt.kubernetes.io/ kubernetes-xenial main\" >> /etc/apt/sources.list.d/kubernetes.list"
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubernetes-cni
-
-# Add br-int and configure its port
-#sudo ovs-vsctl add-br br-int
-#sudo ovs-vsctl set bridge br-int protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13
-#sudo ovs-vsctl add-port br-int enp0s8
-#sudo ip addr flush dev enp0s8
-#sudo ifconfig br-int ${HOST_IP} up
-#sudo ovs-vsctl set-manager tcp:${ODL_IP}:6640
-#sudo ovs-vsctl set-controller br-int tcp:${ODL_IP}:6653
