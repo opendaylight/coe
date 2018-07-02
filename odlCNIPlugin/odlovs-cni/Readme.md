81d291fc2529c407d1d4e8bf57169d8c6fdc3cd5
Frederick F. Kautz IV <fkautz@redhat.com>
2018-07-02 12:45:15 -0700

    Switching watcher from glide to dep
    Signed-off-by: Frederick F. Kautz IV <fkautz@redhat.com>
    Change-Id: Ib61c163a8233e57bd5de94cd452901aea58372e1

--- odlCNIPlugin/odlovs-cni/Readme.md
+++ odlCNIPlugin/odlovs-cni/Readme.md
@@ -1,8 +1,8 @@
-We assume you already installed golang and glide. If not check the below links for more info
+We assume you already installed golang and dep If not check the below links for more info
 - install golang
  https://golang.org/doc/install
-- install glide
- https://github.com/Masterminds/glide
+- install dep
+ https://github.com/golang/dep
 
 Run ./build.sh script it will download the dependencies and build odlovs-cni. The odlovs-cni
 binary will be under the bin directory.
--- odlCNIPlugin/odlovs-cni/build.sh
+++ odlCNIPlugin/odlovs-cni/build.sh
@@ -1,26 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-Update=$1
-
-export GOPATH=$PWD/vendor
-echo "GOPATH=" $GOPATH
-
-if [ ! -d "vendor/src" ] || [ "$Update" = "update" ]; then
-    mkdir -p bin
-    mkdir -p vendor/src
-
-    glide update
-    # glide is wired, create the src dir and move dependencies under it.
-    mkdir vendor/src
-    for dir in vendor/*; do
-      if [ "$dir" != "vendor/src" ]; then
-         cp -r $dir vendor/src;
-      fi;
-      done
-
-    # duplicating the library confuse go at the build.
-    rm -rf vendor/src/github.com/containernetworking/plugins/vendor/github.com/containernetworking
-    rm -rf vendor/src/github.com/containernetworking/plugins/vendor/github.com/vishvananda
-fi
+mkdir -p bin
+dep ensure
 go build -o bin/odlovs-cni
 
